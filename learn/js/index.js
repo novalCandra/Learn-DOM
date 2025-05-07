821c68aeb7af0a9422ad1c8ef0a6f8cbaf998d38
sabcan <dinatanovel5@gmail.com>
2025-05-07 19:30:47 +0700

    Three Day

--- learn/js/index.js
+++ learn/js/index.js
@@ -1,22 +1,23 @@
-const allImages = document.getElementsByTagName('img');
+// const allImages = document.getElementsByTagName('img');
 
-for (let img of allImages) {
-	// console.log(img.src);
-	img.src =
-		'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/1920px-Cat_August_2010-4.jpg';
-}
+// for (let img of allImages) { // Mendapatkan nilai tag Img yang ada di varibale Allimages
+// 	console.log(img.src);
+// 	img.src =
+// 		'https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_S.jpg'; // Merubah Values
+// }
 
 const squareImages = document.getElementsByClassName('square');
 
 for (let img of squareImages) {
+	// console.log(img.src)
 	img.src =
-		'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/1920px-Cat_August_2010-4.jpg';
-}
+		'https://www.allaboutbirds.org/news/wp-content/uploads/2024/05/438159401-Prothonotary_Warbler-Ryan_Justice-FI-720x540.jpg'
+};
 
-const links = document.querySelectorAll('a');
+// const links = document.querySelectorAll('a');
 
-for (let link of links) {
-	link.style.color = 'rgb(0, 108, 134)';
-	link.style.textDecorationColor = 'magenta';
-	link.style.textDecorationStyle = 'wavy';
-}
+// for (let link of links) {
+// 	link.style.color = 'rgb(0, 108, 134)';
+// 	link.style.textDecorationColor = 'magenta';
+// 	link.style.textDecorationStyle = 'wavy';
+// }
