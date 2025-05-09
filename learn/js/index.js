d08e18dbfd87d50956a230a6dcacb8e8b86e9bc9
sabcan <dinatanovel5@gmail.com>
2025-05-10 05:35:19 +0700

    perubahan img

--- learn/js/index.js
+++ learn/js/index.js
@@ -6,18 +6,32 @@
 // 		'https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_S.jpg'; // Merubah Values
 // }
 
-const squareImages = document.getElementsByClassName('square');
+// const squareImages = document.getElementsByClassName('square');
 
-for (let img of squareImages) {
-	// console.log(img.src)
-	img.src =
-		'https://www.allaboutbirds.org/news/wp-content/uploads/2024/05/438159401-Prothonotary_Warbler-Ryan_Justice-FI-720x540.jpg'
-};
+// for (let img of squareImages) {
+// 	// console.log(img.src)
+// 	img.src =
+// 		'https://www.allaboutbirds.org/news/wp-content/uploads/2024/05/438159401-Prothonotary_Warbler-Ryan_Justice-FI-720x540.jpg'
+// };
 
 // const links = document.querySelectorAll('a');
 
 // for (let link of links) {
+// 	// console.log(link.href)
 // 	link.style.color = 'rgb(0, 108, 134)';
 // 	link.style.textDecorationColor = 'magenta';
 // 	link.style.textDecorationStyle = 'wavy';
+// }	
+
+
+// const images = document.querySelectorAll('.square');
+// // console.log(images);
+
+// for(let img of images){
+// 	img.src = 'https://tx.audubon.org/sites/default/files/styles/bean_wysiwyg_full_width/public/cbcpressroom_tuftedtitmouse-judyhowle.jpg?itok=VMtDnqip';
 // }
+
+// const jumlah_foto = images.length;
+// for(let j = 0 ; j <= jumlah_foto; j++){
+// 	console.log(`Jumlah Foto ynag ada Perubahan nya : ${j}`);
+// };
